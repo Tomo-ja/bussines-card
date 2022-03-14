9b1ba0d27f8ac90e0b657cff5a657d2690c741f6
Tomo-ja <tomonao.hj@gmail.com>
2022-03-13 20:24:56 -0700

    done

--- src/compornets/Profile.js
+++ src/compornets/Profile.js
@@ -1,8 +1,16 @@
+import emailLogo from '../images/icon_email.svg'
+import linkedInLogo from '../images/icon_linkedin.svg'
+
 function Profile (){
 	return(
 		<section className="profile">
 			<h1>Tomo Hashiguchi</h1>
 			<h3>Junior Developer</h3>
+			<p className="web-link">tomoano.website</p>
+			<div>
+				<p className="btn"><img src={emailLogo}/>Email</p>
+				<p className="btn"><img src={linkedInLogo}/>LinkedIn</p>
+			</div>
 		</section>
 	)
 }
